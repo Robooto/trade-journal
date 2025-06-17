07640d50161e7c716162e01dd71ea9fbfba91f0f
Sam Russell <samrussell63@gmail.com>
2025-06-16 18:33:13 -0700

    Use CSS overflow for note previews

--- ui/src/app/journal/journal-entry-list/journal-entry-list.component.ts
+++ ui/src/app/journal/journal-entry-list/journal-entry-list.component.ts
@@ -1,5 +1,5 @@
 // src/app/journal-entry-list/journal-entry-list.component.ts
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import { JournalEntry } from '../journal.models';
 
 @Component({
@@ -8,16 +8,13 @@ import { JournalEntry } from '../journal.models';
   styleUrls: ['./journal-entry-list.component.scss'],
   standalone: false,
 })
-export class JournalEntryListComponent implements OnInit {
+export class JournalEntryListComponent {
 
   @Input() entries: JournalEntry[] = [];
   @Output() entrySelected = new EventEmitter<JournalEntry>();
 
   onPanelOpen(entry: JournalEntry) {
     this.entrySelected.emit(entry);
-  }
-
-  ngOnInit(): void {
   }
 
   copyEntry(entry: JournalEntry) {
