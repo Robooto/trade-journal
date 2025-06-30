02b53055df2f4820e31c6ca99d5f54644027e5ae
Sam Russell <samrussell63@gmail.com>
2025-06-29 17:43:17 -0700

    Include timestamp with Hiro screenshots and enhance UI

--- ui/src/app/hiro/hiro-page/hiro-page.component.ts
+++ ui/src/app/hiro/hiro-page/hiro-page.component.ts
@@ -2,23 +2,32 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 
+interface HiroResponse {
+  timestamp: string;
+  images: string[];
+}
+
 @Component({
   selector: 'app-hiro-page',
   templateUrl: './hiro-page.component.html',
   styleUrls: ['./hiro-page.component.scss'],
   standalone: false,
 })
+
 export class HiroPageComponent {
-  images: string[] = [];
+  screens: { timestamp: string; images: string[] }[] = [];
   loading = false;
 
   constructor(private http: HttpClient) {}
 
   getScreens() {
     this.loading = true;
-    this.http.get<{images: string[]}>(`${environment.apiUrl}/spotgamma/hiro`).subscribe({
+    this.http.get<HiroResponse>(`${environment.apiUrl}/spotgamma/hiro`).subscribe({
       next: res => {
-        this.images.push(...res.images.map(img => 'data:image/png;base64,' + img));
+        this.screens.unshift({
+          timestamp: res.timestamp,
+          images: res.images.map(img => 'data:image/png;base64,' + img)
+        });
         this.loading = false;
       },
       error: () => {
